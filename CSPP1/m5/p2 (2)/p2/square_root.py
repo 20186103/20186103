5fe8145240704fbbf10a6c48b90e8529b9bd60bc
20186103 <bhavaniakavarapu526@msitprogram.net>
2018-08-03 18:44:28 +0530

    approx

---
+++ CSPP1/m5/p2 (2)/p2/square_root.py
@@ -0,0 +1,23 @@
+# Write a python program to find the square root of the given number
+# using approximation method
+
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+
+# testcase 2
+# input: 49
+# output: 6.999999999999991
+
+"'to find the square root of the given number using approximation method'"
+def main():
+    """using approximation method"""
+    given_number = int(input())
+    epsilon = 0.01
+    increment = 0.1
+    guess_value = 0.0
+    while abs((guess_value ** 2) - given_number) >= epsilon and guess_value <= given_number:
+        guess_value += increment
+    print(guess_value)
+if __name__ == "__main__":
+    main()
