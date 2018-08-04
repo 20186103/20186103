a1d2db35e02fcfb10c8560d0cca7d9c7e2c33657
20186103 <bhavaniakavarapu526@msitprogram.net>
2018-08-04 12:41:25 +0530

    digit

--- CSPP1/m6/p3 (4)/p3/digit_product.py
+++ CSPP1/m6/p3 (4)/p3/digit_product.py
@@ -9,6 +9,17 @@ def main():
     Read any number from the input, store it in variable int_input.
     '''
     int_input = int(input())
+    product = 1
+    if int_input == 0:
+        product = 0
+    if int_input < 0:
+        int_input = -int_input
+        product = -product
+    while int_input != 0:
+        rem = (int_input%10)
+        product = (product * rem)
+        int_input = int_input//10
+    print(product)
 
 
 if __name__ == "__main__":
