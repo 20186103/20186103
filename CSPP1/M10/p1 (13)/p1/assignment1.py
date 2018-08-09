74d077edb01a132001f9702ce8114cc8453f4c81
20186103 <bhavaniakavarapu526@msitprogram.net>
2018-08-09 16:55:58 +0530

    dictionary

--- CSPP1/M10/p1 (13)/p1/assignment1.py
+++ CSPP1/M10/p1 (13)/p1/assignment1.py
@@ -5,19 +5,19 @@ a list of letters, letters_guessed. This function returns a string
 that is comprised of lowercase English letters - all lowercase English letters
 that are not in letters_guessed
 '''
-
+import string
+DICTIONARY = string.ascii_lowercase
 def get_available_letters(letters_guessed):
     '''
     :param letters_guessed: list, what letters have been guessed so far
     returns: string, comprised of letters that represents what letters have not
       yet been guessed.
     '''
-    alphabets = "abcdefghijklmnopqrstuvwxyz"
-    new_series = ''
-    for i in alphabets:
-        if i not in letters_guessed:
-            new_series = new_series + i
-    return new_series
+    s_t = ''
+    for char in DICTIONARY:
+        if char not in letters_guessed:
+            s_t = s_t + char
+    return s_t
 
 
 
